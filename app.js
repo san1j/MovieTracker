2721424ae6615e66fcb28ecd5c29a0cf825eb3dd
san1j <sanjanasreenath96@gmail.com>
2018-08-27 14:32:23 -0400

    modify port number

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ module.exports = app;
 
 if(!module.parent){ 
 
-var listener = app.listen(process.env.PORT, function() {
+var listener = app.listen(process.env.PORT || 3000, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
 
