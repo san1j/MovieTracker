d496cd053db228acd4875eb3acdf340cc7a08e66
san1j <sanjanasreenath96@gmail.com>
2018-08-27 14:53:09 -0400

    modify db.js to use promises

--- data/db.js
+++ data/db.js
@@ -2,10 +2,9 @@ var MongoClient = require("mongodb").MongoClient;
 if (process.env.NODE_ENV == 'test') {var url = process.env.DB_URI_TEST; var dbname='movie_tracker_test'}
 else {var url = process.env.DB_URI; var dbname='movie_tracker';
 
- (async ()=>{
-    var client = await MongoClient.connect(url,{ useNewUrlParser: true }).catch(error=>console.log("couldn't connect"))
-    module.exports.getdb = client.db(dbname)
-   })()
+  MongoClient.connect(url,{ useNewUrlParser: true })
+  .then(client => module.exports.getdb = client.db(dbname)) 
+  .catch(error => console.log("couldn't connect"))
 
 }
 
